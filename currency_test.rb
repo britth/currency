197518010351836bbd065bf4b7118782e7267470
britth <brittmhayes@gmail.com>
2017-01-10 16:32:45 -0500

    Add test for equal currencies

--- currency_test.rb
+++ currency_test.rb
@@ -1,9 +1,17 @@
-require '/currency.rb'
+require './currency.rb'
 require 'minitest/pride'
 require 'minitest/autorun'
 
 class CurrencyTest < Minitest::Test
   def test_currency_creation
     Currency.new(amount: 34, code: 'USD')
+  end
+
+  def test_equal_currencies
+    a = Currency.new(amount: 34, code: 'USD')
+    b = Currency.new(amount: 34, code: 'USD')
+    assert a == b
+    c = Currency.new(amount: 34, code: 'EUR')
+    refute a == c
   end
 end
